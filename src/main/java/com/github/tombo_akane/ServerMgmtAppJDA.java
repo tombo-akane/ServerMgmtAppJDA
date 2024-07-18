60ee24d4eb686e5d1f6b010f4b2690638f264d87
tombo-akane <175236491+tombo-akane@users.noreply.github.com>
2024-07-18 13:58:29 +0900

    Update ServerMgmtAppJDA.java

--- src/main/java/com/github/tombo_akane/ServerMgmtAppJDA.java
+++ src/main/java/com/github/tombo_akane/ServerMgmtAppJDA.java
@@ -1,6 +1,30 @@
 package com.github.tombo_akane;
 
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
 public class ServerMgmtAppJDA {
     protected static ServerMgmtAppJDA selfBot;
-    public ServerMgmtAppJDA(String token) {}
+    private ShardManager shardManager = null;
+    public ServerMgmtAppJDA(String token) {
+        try {
+            shardManager = buildShardManager(token);
+        } catch (LoginException e) {
+            System.out.println("Failed to start bot! Please check the console for any errors.");
+            System.exit(0);
+        }
+    }
+
+    private ShardManager buildShardManager(String token) throws LoginException {
+        DefaultShardManagerBuilder builder =
+                DefaultShardManagerBuilder.createDefault(token);
+
+        return builder.build();
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
 }
