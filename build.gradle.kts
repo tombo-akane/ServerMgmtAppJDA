1f3dd87770bb7a11379d61a4e704715a29bd0fe1
tombo-akane <175236491+tombo-akane@users.noreply.github.com>
2024-07-18 13:16:57 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.gradle.jvm.tasks.Jar
+
 plugins {
+    idea
+    application
     id("java")
 }
 
 group = "com.github.tombo_akane"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -12,8 +16,32 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:5.0.1")
+    implementation("commons-cli:commons-cli:1.5.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.github.tombo_akane.Main")
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Implementation-Title"] = "ServerMgmtAppJDA"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "com.github.tombo_akane.Main"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+    destinationDirectory.set(layout.buildDirectory.dir("dist"))
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
