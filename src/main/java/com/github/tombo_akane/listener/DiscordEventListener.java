60e345512daa9170a219f980cc7347f4a9ebcdf5
tombo-akane <175236491+tombo-akane@users.noreply.github.com>
2024-07-18 14:08:22 +0900

    Update DiscordEventListener.java

--- src/main/java/com/github/tombo_akane/listener/DiscordEventListener.java
+++ src/main/java/com/github/tombo_akane/listener/DiscordEventListener.java
@@ -1,4 +1,11 @@
 package com.github.tombo_akane.listener;
 
-public class DiscordEventListener {
+import com.github.tombo_akane.ServerMgmtAppJDA;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class DiscordEventListener extends ListenerAdapter {
+    public ServerMgmtAppJDA bot;
+    public DiscordEventListener(ServerMgmtAppJDA bot) {
+        this.bot = bot;
+    }
 }
